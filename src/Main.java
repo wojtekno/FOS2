4d07d55678693244e7fdfd1c835f849849584bcf
wojtekno <wjw.nowak@gmail.com>
2017-09-17 22:53:57 +0200

    last comments added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-
+// Food ordering system 
+// designed to be used by waiters
+// assumption made that you can order a lunch set (main + dessert) or drinks
+// if you want to extend the menu check Menu.class for instructions
 public class Main {
 
 	public static void main(String[] args) {
